2d4f500516dc1dd13560a6ecc1657d16234a8216
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2024-10-05 00:34:05 +0200

    [BUGFIX]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      <head>
-        <script src="https://cdn.jsdelivr.net/npm/reveal.js-plugins@latest/chart/plugin.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js"></script>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
